b7428532a9ffe17a4359c64280d9ed83b597eafa
指尖下的幽灵 <913337456@qq.com>
2019-05-29 21:02:53 +0800

    修改 nonce 管理器

--- nonce_manager.go
+++ nonce_manager.go
@@ -10,7 +10,6 @@ type NonceManager struct {
 	// lock 是互斥锁，go 的 map 类型不是线程安全的，
 	// 在读写 map 的时候，我们要考虑上多协程并发的情况
 	lock sync.Mutex
-
 	// 采用整形大数来存储 nonce
 	nonceMemCache map[string]*big.Int
 }
@@ -19,6 +18,15 @@ func NewNonceManager() *NonceManager {
 	return &NonceManager{
 		lock: sync.Mutex{}, // 实例化互斥锁
 	}
+}
+//  设置 nonce
+func (n *NonceManager) SetNonce(address string,nonce *big.Int) {
+	if n.nonceMemCache == nil {
+		n.nonceMemCache = map[string]*big.Int{}
+	}
+	n.lock.Lock()         // 加锁
+	defer n.lock.Unlock() // 当该函数执行完毕，进行解锁
+	n.nonceMemCache[address] = nonce
 }
 
 // 根据以太坊地址获取 nonce
@@ -39,6 +47,9 @@ func (n *NonceManager) PlusNonce(address string) {
 	n.lock.Lock()         // 加锁
 	defer n.lock.Unlock() // 当该函数执行完毕，进行解锁
 	oldNonce := n.nonceMemCache[address]
+	if oldNonce == nil {
+		return
+	}
 	newNonce := oldNonce.Add(oldNonce, big.NewInt(int64(1)))
 	n.nonceMemCache[address] = newNonce
 }
